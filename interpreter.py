8dc3c8efea0fd2f2458a05049acfea8b346890af
DarkblooM-IO <darkbloompro@gmail.com>
2025-02-21 17:22:18 +0100

    commit

--- interpreter.py
+++ interpreter.py
@@ -6,28 +6,27 @@ from readline import parse_and_bind
 HELPSTRING = """
 ==== Bloomlang esolang ====
 
-You get an accumulator with an initial value of 0 that you can increment or decrement,
-as well as a buffer that can store one value at a time.
-
-The code is read character by character and instructions are executed one after the other.
+You get a value x with an initial value of 0 that you can increment or decrement,
+as well as a value y that can store one value at a time. The code is read character by
+character and instructions are executed one after the other.
 
 Instruction list:
- ^    increment accumulator
- `    decrement accumulator
- !    set accumulator to 0
- +    add buffer to accumulator
- -    substract buffer from accumulator
- *    multiply accumulator by buffer
- /    divide accumulator by buffer and store the result (floor division)
- %    divide accumulator by buffer and store the remainder (modulo)
- =    set buffer to accumulator
- ~    set accumulator to buffer
- #    print accumulator
- @    print accumulator as ASCII
- .    set accumulator to user input
- {}   repeatedly run the code inside while accumulator is not 0
- |    halt the program
- $    (optional, for debugging) print buffer
+ ^    x += 1
+ `    x -= 1
+ !    x = 0
+ +    x += y
+ -    x -= y
+ *    x *= y
+ /    x /= y
+ %    x %= y
+ =    y = x
+ ~    x = y
+ #    print x
+ @    print chr x
+ .    x = input
+ {}   while x != 0 { ... }
+ |    exit
+ $    print y
 
 Any other character is ignored.
 """.strip()
